0e29c523dca036f32401ff5bf45aaef1124add1d
René Smeekes <r.smeekes@gmail.com>
2014-12-03 17:34:56 +0100

    First plot script added

---
+++ plot1.R
@@ -0,0 +1,32 @@
+## Preparation and retrieving data
+workpath <- "exdata-016/p1"
+currwd <- getwd()
+
+if (!file.exists(workpath)) dir.create(workpath, recursive=T)
+
+setwd(workpath)
+
+if (!file.exists("household_power_consumption.txt")) {
+        download.file("http://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip", destfile="exdata-016-p1.zip")
+        unzip("exdata-016-p1.zip")
+}
+
+pcn <- subset(read.csv("household_power_consumption.txt", 
+                       sep=";", na.strings="?", 
+                       colClasses=c("character","character","numeric","numeric","numeric","numeric","numeric","numeric","numeric")
+                       ),
+              Date %in% c("1/2/2007","2/2/2007")
+              )
+pcn$Time <- strptime(paste(pcn$Date, pcn$Time),format="%d/%m/%Y %H:%M:%S")
+pcn$Date <- as.Date(pcn$Date,format="%d/%m/%Y")
+
+
+## creating plot
+
+png("plot1.png")
+hist(pcn$Global_active_power, main="Global Active Power", xlab="Global Active Power (kilowatts)", col="Red")
+dev.off()
+
+## reset wd to what it originally was
+setwd(currwd)
+
